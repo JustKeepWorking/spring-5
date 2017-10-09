e8f561d74a81359e10e40c386a9aeb8f9a38252d
Hai Nguyen <1533478@hcmut.edu.vn>
2017-10-09 23:44:58 +0700

    update repository reactive

--- src/main/java/com/nduyhai/spring5/Spring5Application.java
+++ src/main/java/com/nduyhai/spring5/Spring5Application.java
@@ -1,10 +1,22 @@
 package com.nduyhai.spring5;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.repository.CrudRepository;
 import reactor.core.publisher.Flux;
+
+import java.util.Random;
+import java.util.UUID;
+import java.util.stream.Stream;
+
 
 @SpringBootApplication
 public class Spring5Application {
@@ -14,14 +26,35 @@ public class Spring5Application {
 	}
 
 	@Bean
-    CommandLineRunner demo() {
+    CommandLineRunner demo(MovieRepository repository) {
+	    repository.deleteAll();
+
 	    return args -> {
-            Flux<String> flux = Flux.fromArray("1,2,3,4".split(","));
-            flux.map(Integer::parseInt)
-                    .filter(i -> i % 2 == 0)
-                    .subscribe( System.out::println, null, null);
+            Stream.of("Game of throne", "Enter Mono<Void>", "Winter is coming", "Chao isn't a bitch", "Back to the future")
+                    .map(name -> new Movie(UUID.randomUUID().toString(), name, randomGenre()))
+                    .forEach(movie -> repository.save(movie).subscribe(System.out::println));
 
         };
     }
-	
+
+    private String randomGenre() {
+        String[] genres = "honor,action,drama".split(",");
+        return genres[new Random().nextInt(genres.length)];
+    }
+
+}
+
+interface MovieRepository extends ReactiveMongoRepository<Movie, String> {
+
+}
+
+@Document
+@AllArgsConstructor
+@ToString
+@Data
+class Movie {
+    @Id
+    private String id;
+
+    private String title, genre;
 }
