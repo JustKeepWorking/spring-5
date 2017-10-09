842750cc019726c52a77fff58fe2790462fc51f5
Hai Nguyen <1533478@hcmut.edu.vn>
2017-10-09 23:48:33 +0700

    update repository reactive

--- src/main/java/com/nduyhai/spring5/Spring5Application.java
+++ src/main/java/com/nduyhai/spring5/Spring5Application.java
@@ -27,14 +27,14 @@ public class Spring5Application {
 
 	@Bean
     CommandLineRunner demo(MovieRepository repository) {
-	    repository.deleteAll();
-
-	    return args -> {
+	    return args -> repository.deleteAll()
+        .subscribe(null, null, args -> {
             Stream.of("Game of throne", "Enter Mono<Void>", "Winter is coming", "Chao isn't a bitch", "Back to the future")
                     .map(name -> new Movie(UUID.randomUUID().toString(), name, randomGenre()))
                     .forEach(movie -> repository.save(movie).subscribe(System.out::println));
 
-        };
+        });
+
     }
 
     private String randomGenre() {
